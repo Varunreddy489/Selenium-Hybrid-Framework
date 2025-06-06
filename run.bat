023c5adb771fad9d49a60bd6ad2aa283a0aea0c9
Varunreddy <varunsannapureddy@gmail.com>
2025-06-06 23:24:12 +0530

    fix: Update run.bat to use absolute path for pytest executable

--- run.bat
+++ run.bat
@@ -1,7 +1,7 @@
 REM pytest -v -s -m "sanity" --html=./reports/report.html test_cases/ --browser chrome
 
-pytest -v -s -m "regression" --html=./reports/report.html test_cases/ --browser chrome
-pytest -v -s -m "regression" --html=./reports/report.html test_cases/ --browser firefox
+C:\Users\varun\AppData\Local\Programs\Python\Python312\Scripts\pytest.exe -v -s -m "regression" --html=./reports/report.html test_cases/ --browser chrome
+C:\Users\varun\AppData\Local\Programs\Python\Python312\Scripts\pytest.exe -v -s -m "regression" --html=./reports/report.html test_cases/ --browser firefox
 
 REM pytest -v -s -m "sanity or regression" --html=./reports/report.html test_cases/ --browser chrome
 
