6ae089fafc0d9215b08f0aaa564d0d6098630e83
Varunreddy <varunsannapureddy@gmail.com>
2025-06-06 16:26:34 +0530

    feat: Add SearchEmployee page object and corresponding test case for employee search functionality

---
+++ page_objects/search_employee_page.py
@@ -0,0 +1,41 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+class SearchEmployee:
+    pim_xpath = "//span[normalize-space()='PIM']"
+
+    search_button_xpath = "//button[normalize-space()='Search']"
+
+    employee_list_xpath = "//a[normalize-space()='Employee List']"
+
+    employee_id_input = "//div[@class='oxd-input-group oxd-input-field-bottom-space']//div//input[@class='oxd-input oxd-input--active']"
+
+    test_emp_id = "//div[@class='oxd-table-cell oxd-padding-cell' and @role='cell']/div"
+
+    search_emp_id = "//div[contains(text(),'09557')]"
+
+    def __init__(self, driver):
+        self.driver = driver
+        self.wait = WebDriverWait(driver, 10)
+
+    def click_pim(self):
+        pim = self.wait.until(EC.element_to_be_clickable((By.XPATH, self.pim_xpath)))
+        pim.click()
+
+    def redirect_emp_list(self):
+        self.wait.until(EC.element_to_be_clickable((By.XPATH, self.employee_list_xpath))).click()
+
+    def set_employee_id(self, name):
+        emp_name = self.wait.until(EC.presence_of_element_located((By.XPATH, self.employee_id_input)))
+        emp_name.send_keys(name)
+
+    def search_button(self):
+        self.wait.until(EC.presence_of_element_located((By.XPATH, self.search_button_xpath))).click()
+
+    def search_emp_text(self, emp_id):
+        element = self.wait.until(EC.presence_of_element_located(
+            (By.XPATH, f"//div[contains(text(),'{emp_id}')]")))
+
+        return element.text
---
+++ test_cases/test_search_employee.py
@@ -0,0 +1,60 @@
+import time
+
+import pytest
+
+from page_objects.login_page import Login
+from page_objects.search_employee_page import SearchEmployee
+from utils.custom_logger import LogGen
+from utils.read_props import ReadConfig
+
+
+class TestSearchEmployee_004:
+    baseURL = ReadConfig.get_app_url()
+    username = ReadConfig.get_username()
+    password = ReadConfig.get_password()
+    logger = LogGen.log_gen()
+
+    @pytest.mark.regression
+    def test_search_emp(self, setup):
+        self.logger.info("********** Starting Add Employee **********")
+        driver = setup
+        driver.get(self.baseURL)
+
+        lp = Login(driver)
+        se = SearchEmployee(driver)
+
+        try:
+            self.logger.info("********** Started Login **********")
+
+            lp.set_username(self.username)
+            lp.set_password(self.password)
+            lp.click_login()
+
+            self.logger.info("********** Completed Login **********")
+
+            se.click_pim()
+            se.redirect_emp_list()
+
+            # se.set_employee_name("Emily Jones")
+            se.set_employee_id("0295")
+            se.search_button()
+
+            time.sleep(5)
+
+            test = se.search_emp_text("0295")
+            print("scdascdsddnvv", test)
+
+            assert test == "0295"
+
+
+        except Exception as e:
+            timestamp = time.strftime("%Y%m%d-%H%M%S")
+            screenshot_path = f"C:\\Users\\varun\\OneDrive\\Desktop\\GE\\WEB AUTOMATION\\nopcommerce\\screenshots\\test_homepage_{timestamp}.png"
+            time.sleep(3)
+            driver.save_screenshot(screenshot_path)
+            self.logger.error(f"Test Search Employee failed: {e}")
+            # traceback.print_exc()
+            assert False
+
+        finally:
+            driver.close()
