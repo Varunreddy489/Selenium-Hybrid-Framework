9db18aee570113217c74968ef8155eb4ffc18ace
Varunreddy <varunsannapureddy@gmail.com>
2025-06-07 10:15:56 +0530

    fix: Comment out unused pytest command for Firefox in run.bat

--- run.bat
+++ run.bat
@@ -1,7 +1,8 @@
 REM pytest -v -s -m "sanity" --html=./reports/report.html test_cases/ --browser chrome
 
 C:\Users\varun\AppData\Local\Programs\Python\Python312\Scripts\pytest.exe -v -s -m "regression" --html=./reports/report.html test_cases/ --browser chrome
-C:\Users\varun\AppData\Local\Programs\Python\Python312\Scripts\pytest.exe -v -s -m "regression" --html=./reports/report.html test_cases/ --browser firefox
+
+REM C:\Users\varun\AppData\Local\Programs\Python\Python312\Scripts\pytest.exe -v -s -m "regression" --html=./reports/report.html test_cases/ --browser firefox
 
 REM pytest -v -s -m "sanity or regression" --html=./reports/report.html test_cases/ --browser chrome
 
