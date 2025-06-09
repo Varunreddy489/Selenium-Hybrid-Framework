347ad4cab52c3264607152c9cf1d741ebad7dae7
Varunreddy <varunsannapureddy@gmail.com>
2025-06-09 15:30:35 +0530

    fix: Add pytest command for combined sanity and regression tests in run.bat

--- run.bat
+++ run.bat
@@ -2,8 +2,9 @@ REM C:\Users\varun\AppData\Local\Programs\Python\Python312\Scripts\pytest.exe -v
 
 REM C:\Users\varun\AppData\Local\Programs\Python\Python312\Scripts\pytest.exe -v -s -m "regression" --html=./reports/report.html test_cases/ --browser chrome
 
+C:\Users\varun\AppData\Local\Programs\Python\Python312\Scripts\pytest.exe -v -s -m "sanity or regression" --html=./reports/report.html test_cases/ --browser chrome
+
 REM C:\Users\varun\AppData\Local\Programs\Python\Python312\Scripts\pytest.exe -v -s -m "regression" --html=./reports/report.html test_cases/ --browser firefox
 
-C:\Users\varun\AppData\Local\Programs\Python\Python312\Scripts\pytest.exe -v -s -m "sanity or regression" --html=./reports/report.html test_cases/ --browser chrome
 
 REM C:\Users\varun\AppData\Local\Programs\Python\Python312\Scripts\pytest.exe -v -s -m "sanity and regression" --html=./reports/report.html test_cases/ --browser chrome
